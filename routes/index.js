e67aa84de7f396f23efd624f507613710ab4d444
abhi20012 <kashyapabhinav777@gmail.com>
2023-09-13 15:20:33 +0530

    creating controller and respective routes

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,16 @@
 const express = require('express');
 
+//creating router 
 const router = express.Router();
+
+//controllers 
+const homeController = require('../controllers/home_controller');
+
+router.get('/', homeController.home);
+
+//router to get to users controllers
+router.use('/users', require('./users'));
+
 
 console.log('Router loaded');
 
