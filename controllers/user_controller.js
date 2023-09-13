b439af4b839ac93993082f239a8f85d889097b4f
abhi20012 <kashyapabhinav777@gmail.com>
2023-09-13 16:33:03 +0530

    Created the user login/signup pages

--- controllers/user_controller.js
+++ controllers/user_controller.js
@@ -12,11 +12,19 @@ module.exports.signUp = function(req, res){
 	})
 }
 
-
-
 //rendering the signin page
-module.exports.signIp = function(req, res){
+module.exports.signIn = function(req, res){
 	return res.render('user_sign_in', {
 		title:"Sign In page"
 	})
+}
+
+//get the sign up data
+module.exports.create = function(req, res){
+	//TODO later
+}
+
+// sign in and create a session for the user
+module.exports.createSession = function(req, res){
+	//TODO later
 }
